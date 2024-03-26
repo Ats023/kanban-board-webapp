a3c6265f01c478b3a3e450e3a25e10f6c00d652a
Atryee Dhar <atryeedhar23@gmail.com>
2024-03-26 21:17:09 +0530

    Improved styling, automatic updation of task points

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
+//PROGRESS: 1.04.18
 import { useState } from 'react'
 import './App.css'
 import TaskCard from './components/TaskCard'
-import { Task, Status, tasks, statuses } from './utils/data-tasks'
+import { Task, Status, tasks as initialTasks, statuses } from './utils/data-tasks'
 
 function App() {
- const columns = statuses.map((status) => {
+  const [tasks, setTasks] = useState<Task[]>(initialTasks)
+  const columns = statuses.map((status) => {
   const tasksInColumn = tasks.filter((task) => task.status === status)
   return {
     status,
     tasks: tasksInColumn,
   }
  })
+
+ const updateTaskPoints = (task: Task, points: number) => {
+  const updatedTasks = tasks.map((t) => {
+    if(t.id === task.id) return {
+      ...t,
+      points,
+    }
+    else return t
+  })
+  setTasks(updatedTasks)
+ }
+
   return (
     <>
-    <h1 className="text-4xl font-semibold py-4 px-2">Kanban-Board</h1>
+    <h1 className="text-4xl font-bold py-4 px-2 text-slate-700">Kanban-Board</h1>
     <div className='flex divide-x justify-center'>
       {columns.map((column) => (
         <div className='px-2'>
-          <h2 className="text-2xl font-semibold py-2">{column.status}</h2>
-          {column.tasks.map((task) => <TaskCard task = {task} />)}
+          <h2 className="text-2xl font-bold py-1 text-gray-800">{column.status}</h2>
+          <p className="font-bold text-indigo-400">Total: {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}</p>
+          {column.tasks.map((task) => 
+          <TaskCard task = {task} updateTaskPoints={updateTaskPoints} />
+          )}
         </div>
       ))}
     </div>
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,19 +2,22 @@ import { useState } from "react"
 import { Task } from "../utils/data-tasks"
 
 
-const TaskCard = ({task}: {
+const TaskCard = ({task, updateTaskPoints}: {
     task: Task
+    updateTaskPoints: (task: Task, points: number) => void
 }) => {
-    const [points, setPoints] = useState(task.points)
+    //const [points, setPoints] = useState(task.points || 0)
+    const points = task.points || 0
     return <div className="border rounded-lg p-2 m-2 w-64">
-        <div className="text-2xl font-semibold">{task.title}</div>
+        <div className="text-2xl font-semibold text-gray-900">{task.title}</div>
         <div className="flex gap-4 justify-between">
-            <div>
+            <div className="font-bold text-slate-600">
                 {task.id}
             </div>
-            <div>
-                {points}
-                <button onClick={(e)=>setPoints(points + 1)}>+</button>
+            <div className="flex gap-2 items-center font-bold text-slate-600">
+                <button className="text-red-500" onClick={(e)=>updateTaskPoints(task, points - 1)}>-</button>
+                <div className="text-indigo-400">{points}</div>
+                <button className="text-green-500" onClick={(e)=>updateTaskPoints(task, points + 1)}>+</button>
             </div>
         </div>
     </div>
